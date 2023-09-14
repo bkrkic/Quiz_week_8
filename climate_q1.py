64a693b2a6eaa59ca008941595d07221bb497a11
bkrkic91 <begokrkic@gmail.com>
2023-09-14 16:44:41 +1000

    wk8 comp

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,29 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+import datetime
+connection = sqlite3.connect('climate.db')
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM ClimateData")
+results = cursor.fetchall()
+# Check results - database table
+print(results)
+
 years = []
 co2 = []
 temp = []
+
+for result in results:
+    years.append(result[0])
+    co2.append(result[1])
+    temp.append(result[2])
+# Check arrays
+print('years: ', years, 'co2: ', co2, 'temp: ', temp)
+
+
+# years = []
+# co2 = []
+# temp = []
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -13,6 +34,8 @@ plt.xlabel("Year (decade)")
 plt.subplot(2, 1, 2)
 plt.plot(years, temp, 'r*-') 
 plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
+plt.xlabel("Year (decade)")
+# CORRECTION MADE: Change order to perform save action
+plt.savefig("co2_temp_1.png")
 plt.show() 
-plt.savefig("co2_temp_1.png") 
+
--- climate_q2.py
+++ climate_q2.py
@@ -1,9 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+df = pd.read_csv(r"climate.csv")
+
+# Most efficient method - tolist()
+years = df['Year'].tolist()
+co2 = df['CO2'].tolist()
+temp = df['Temperature'].tolist()
+
+# years = []
+# co2 = []
+# temp = []
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -14,7 +21,8 @@ plt.xlabel("Year (decade)")
 plt.subplot(2, 1, 2)
 plt.plot(years, temp, 'r*-') 
 plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
+plt.xlabel("Year (decade)")
+# CORRECTION MADE: Change order to perform save action
+plt.savefig("co2_temp_2.png")
+plt.show()
 
